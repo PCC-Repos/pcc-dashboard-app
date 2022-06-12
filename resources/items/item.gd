575783fa892b63a42dbe06e24abbeb081b07984e
sairam4123 <kannanscoffee@outlook.com>
2022-06-12 22:26:05 +0530

    Add basic transactions UI. * Remove API submodule. * Fix market_place items unable to show themselves properly.

--- resources/items/item.gd
+++ resources/items/item.gd
@@ -1,15 +1,33 @@
 extends MouseButton
 
 
-var item_name: String
-var item_text: String
-var item_price: String
+export var item_name: String setget set_item_name
+export var item_text: String setget set_item_text
+export var item_price: int setget set_item_price
 
 onready var item_name_lbl = $"%Name"
 onready var item_desc_lbl = $"%Description"
 onready var item_price_lbl = $"%Price"
 
 func _ready():
+	set("item_name", item_name)
+	set("item_text", item_text)
+	set("item_price", item_price)
+
+func set_item_name(value: String):
+	item_name = value
+	if !is_inside_tree():
+		return
 	item_name_lbl.text = item_name
+
+func set_item_text(value: String):
+	item_text = value
+	if !is_inside_tree():
+		return
 	item_desc_lbl.text = item_text
+
+func set_item_price(value: int):
+	item_price = value
+	if !is_inside_tree():
+		return
 	item_price_lbl.text = str(item_price) + " tokens"
---
+++ resources/transactions/transaction.gd
@@ -0,0 +1,58 @@
+tool
+extends Button
+
+
+export var buyer_name: String setget set_buyer_name
+export var seller_name: String setget set_seller_name
+export var item_name: String setget set_item_name
+export var price: String setget set_price
+
+var buyer setget set_buyer
+var seller setget set_seller
+var item setget set_item
+
+func _ready():
+	set("buyer_name", buyer_name)
+	set("seller_name", seller_name)
+	set("item_name", item_name)
+	set("price", price)
+	
+
+func set_buyer_name(value: String):
+	buyer_name = value
+	if !is_inside_tree():
+		return
+	$"%Buyer".text = buyer_name
+
+
+func set_seller_name(value: String):
+	seller_name = value
+	if !is_inside_tree():
+		return
+	$"%Seller".text = seller_name
+
+
+func set_item_name(value: String):
+	item_name = value
+	if !is_inside_tree():
+		return
+	$"%Item".text = item_name
+
+
+func set_price(value: String):
+	price = value
+	if !is_inside_tree():
+		return
+	$"%Price".text = price + " tokens"
+
+func set_buyer(value):
+	buyer = value
+	self.buyer_name = value["name"]
+
+func set_seller(value):
+	seller = value
+	self.seller_name = value["name"]
+
+func set_item(value):
+	item = value
+	self.item_name = value["name"]
