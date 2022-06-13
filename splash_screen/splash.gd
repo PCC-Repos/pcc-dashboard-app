c4a18a4580b71d1afd0f5f461f3913f096e8059a
sairam4123 <kannanscoffee@outlook.com>
2022-06-13 19:10:07 +0530

    Implement transactions (75% done). * Enable HiDpi for themes. * Replace project icons. * Add new title font. * Add some VSeperators to the Transaction.tscn. * Try fix the unable to click mouse issue (fail). * Replace the splash screen pre-animation. (Controvesial)

--- splash_screen/splash.gd
+++ splash_screen/splash.gd
@@ -7,7 +7,7 @@ func _ready():
 	OS.request_permissions()
 	get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_2D, SceneTree.STRETCH_ASPECT_EXPAND, Vector2(768, 768))
 	$TextureRect.show()
-	$AnimationPlayer.play("show_img_rect")
+	$AnimationPlayer.play("hide_img_rect")
 	$AudioStreamPlayer.play()
 
 
